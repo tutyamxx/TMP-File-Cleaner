94f61677b82f88cfefd51f8072d150dff2e86187
tutyamxx <tutygoo@gmail.com>
2017-11-25 20:27:39 +0000

    Initial Commit

---
+++ TMP File Cleaner/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace TMP_File_Cleaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_ToolName = new System.Windows.Forms.Label();
+            this.btn_CleanTmp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_ToolName
+            // 
+            this.lbl_ToolName.AutoSize = true;
+            this.lbl_ToolName.Font = new System.Drawing.Font("GeForce", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ToolName.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lbl_ToolName.Location = new System.Drawing.Point(68, 145);
+            this.lbl_ToolName.Name = "lbl_ToolName";
+            this.lbl_ToolName.Size = new System.Drawing.Size(125, 21);
+            this.lbl_ToolName.TabIndex = 2;
+            this.lbl_ToolName.Text = "TMP File Cleaner";
+            // 
+            // btn_CleanTmp
+            // 
+            this.btn_CleanTmp.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btn_CleanTmp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_CleanTmp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CleanTmp.Font = new System.Drawing.Font("GeForce", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_CleanTmp.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.btn_CleanTmp.Location = new System.Drawing.Point(72, 220);
+            this.btn_CleanTmp.Name = "btn_CleanTmp";
+            this.btn_CleanTmp.Size = new System.Drawing.Size(121, 38);
+            this.btn_CleanTmp.TabIndex = 0;
+            this.btn_CleanTmp.Text = "Clean Temporary Files";
+            this.btn_CleanTmp.UseVisualStyleBackColor = false;
+            this.btn_CleanTmp.Click += new System.EventHandler(this.btn_CleanTmp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("GeForce", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(57, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "(Temporary Files Cleaner)";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TMP_File_Cleaner.Properties.Resources.wipe;
+            this.pictureBox3.Location = new System.Drawing.Point(81, 56);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(68, 69);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TMP_File_Cleaner.Properties.Resources.cuteclean;
+            this.pictureBox2.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 113);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TMP_File_Cleaner.Properties.Resources.trash;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 59);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(267, 270);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lbl_ToolName);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_CleanTmp);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(366, 432);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TMP File Cleaner v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_CleanTmp;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lbl_ToolName;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ TMP File Cleaner/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TMP_File_Cleaner
+{
+    public partial class Form1 : Form
+    {
+        long TempFilesSize = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*public static long GetDirectorySize(string parentDirectory)
+        {
+            return new DirectoryInfo(parentDirectory).GetFiles("*.*", SearchOption.AllDirectories).Sum(file => file.Length);
+        }
+        */
+
+        static long DirectorySize(DirectoryInfo dInfo, bool includeSubDir)
+        {
+            // Enumerate all the files
+            long totalSize = dInfo.EnumerateFiles().Sum(file => file.Length);
+
+            // If Subdirectories are to be included
+            if (includeSubDir)
+            {
+                // Enumerate all sub-directories
+                totalSize += dInfo.EnumerateDirectories().Sum(dir => DirectorySize(dir, true));
+            }
+            return totalSize;
+        }
+
+        static String BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+
+            if (byteCount == 0)
+            {
+                return "0 " + suf[0];
+            }
+
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+
+            return (Math.Sign(byteCount) * num).ToString() + " " + suf[place];
+        }
+
+        private void btn_CleanTmp_Click(object sender, EventArgs e)
+        {
+            TempFilesSize = 0;
+
+            DialogResult dialogResult = MessageBox.Show("Clean all temporary files?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dialogResult == DialogResult.Yes)
+            {
+                // --| This is basic "Temp" folder in every Windows Operating System!
+                // --| Many users do not know that it contains temporary files that are stored in this folder in time, and can really fill your HDD space!
+                // --| This folder can be accessed by typing "%temp%" or "%appdata%" into the search bar. Because is so tricky many users don't know about it!
+                string szTemporaryFilesPath = Path.GetTempPath();
+                var Dir = new DirectoryInfo(szTemporaryFilesPath);
+
+                if (!Dir.Exists)
+                {
+                    MessageBox.Show("Welp... 'Temp' folder is missing somehow!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                    return;
+                }
+
+                foreach (DirectoryInfo dir in Dir.GetDirectories())
+                {
+                    try
+                    {
+                        TempFilesSize += DirectorySize(dir, true);
+                        dir.Delete(true);
+                    }
+                    
+                    catch (Exception)
+                    {
+                        // Ignore folders that are locked or being used.
+                    }
+                }
+
+                foreach (FileInfo szFile in Dir.GetFiles())
+                {
+                    try
+                    {
+                        TempFilesSize += szFile.Length;
+                        szFile.Delete();
+                    }
+
+                    catch (Exception)
+                    {
+                        // Ignore files that are locked or being used.
+                    }
+                       
+                }
+
+                MessageBox.Show("Temporary files have been deleted!\nThis time you cleaned " + BytesToString(TempFilesSize) + "!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                TempFilesSize = 0;
+            }
+
+            else if (dialogResult == DialogResult.No)
+            {
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+        }
+    }
+}
